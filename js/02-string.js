3a1d36bd5bd1b6327d5c734c6d7e5ce64fb40cf1
JustEdga <114071432+JustEdga@users.noreply.github.com>
2022-10-18 11:42:08 +0300

    Numbers , String

---
+++ js/02-string.js
@@ -0,0 +1,78 @@
+const firstName = 'Petras';
+console.log(firstName);
+
+const lastName = 'Petraitis';
+console.log(lastName);
+
+const fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+const kabutes = 'Cia tures buti 2 kabutes.';
+console.log(kabutes);
+
+const kabutes2 = 'Dvyguba kabute (").';
+console.log(kabutes2);
+
+const kabutes1 = "Vienguba kabute (').";
+console.log(kabutes1);
+
+const kabutes11 = 'Vienguba (\') ir dvyguba (") kabutes.';
+console.log(kabutes11);
+
+
+const kabuteSplit = "Vienguba (') ir" + ' dvyguba (") kabutes.';
+console.log(kabuteSplit);
+
+const multiline = 'Labas\n\r\
+rytas, \
+Lietuva!';
+console.log(multiline);
+
+const HTML = '<header>\n\r\
+    <img>\n\r\
+    <nav>\n\r\
+        <a></a>\n\r\
+</header>'
+console.log(HTML);
+
+const backtip = `Kabute.`;
+console.log(backtip);
+
+const backtip12 = `Vienguba (') ir dvyguba (") kabutes.`;
+console.log(backtip12);
+
+const backtip123 = `Vienguba (') ir dvyguba (") ir backtip (\`).`;
+console.log(backtip123);
+
+const HTML2 = `<header>
+    <img>
+    <nav>
+        <a></a>
+</header>`
+console.log(HTML2);
+
+console.log('----------');
+
+// Hi there, Chuck!
+
+const username = 'Chuck';
+const hi = 'Hi there, ' + username + '!';
+console.log(hi);
+
+const hi2 = "Hi there, " + username + "!";
+console.log(hi2);
+
+const hiBacktik = `HI there, ${username}!`;
+console.log(hiBacktik);
+
+const n1 = 7;
+const n2 = 5;
+const sum = n1 + n2;
+
+const sumString1 = `${n1} + ${n2} = ${sum}`;
+console.log(sumString1);
+
+const sumString2 = `${n1} + ${n2} = ${n1 + n2}`;
+console.log(sumString2);
+
+
